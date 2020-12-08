3bea0544d57597e73d971742b3e40da1f199f137
nalapatt <70668968+nalapatt@users.noreply.github.com>
2020-12-08 16:37:02 -0500

    Update MostRepeatedWord.java

--- MostRepeatedWord.java
+++ MostRepeatedWord.java
@@ -15,29 +15,14 @@ import java.io.BufferedReader;
     	
 		ArrayList<String> articles = new ArrayList<String>(Arrays.asList("the", "a", "an"));
          	 System.out.println("after initialize");
-/*         	try(FileReader fileReader =new FileReader("c:\\data\\text.txt")){
-
-    int data = fileReader.read();
-    while(data != -) {
-        System.out.print((char) data));
-        data = fileReader.read();
-    }
-}*/
 		//Opens file in read mode and prints out each character 
           FileReader fr=new FileReader("/home/ubuntu/recipe.txt"); 
 	  String data = fr.read();
 	 while(data != null) 
 	{
-        System.out.print(data));
+        System.out.print(data);
         data = fr.read();
     	}
-	  /*while((i=fr.read())!=-1) 
-	  {
-	
-               System.out.print((char)i);
-		   System.out.println("integer");
-          }
-		*/
 	  fr.close();    
 }    
 	
