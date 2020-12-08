5b334e2f7590a8a579675eca3431cc5c4fcebee5
nalapatt <70668968+nalapatt@users.noreply.github.com>
2020-12-08 15:17:32 -0500

    Update MostRepeatedWord.java

--- MostRepeatedWord.java
+++ MostRepeatedWord.java
@@ -6,48 +6,23 @@ import java.io.BufferedReader;
 	public class MostRepeatedWord {  
 	
      	 
-    	public static void main(String[] args) throws Exception {  
+    	public static void main(String[] args) throws Exception { 
+		
         	String line, word = "";  
         	int count = 0, maxCount = 0;  
 		System.out.println("Inside main ");
         	ArrayList<String> words = new ArrayList<String>();  
-    	ArrayList<String> articles = new ArrayList<String>(Arrays.asList("the", "a", "an"));
+    	
+		ArrayList<String> articles = new ArrayList<String>(Arrays.asList("the", "a", "an"));
          	 System.out.println("after initialize");
-        	//Opens file in read mode  
-        	FileReader file = new FileReader(args[0]);  
-        	BufferedReader br = new BufferedReader(file);  
-         	 System.out.println("after initialize 1");
-        	//Reads each line  
-        	while((line = br.readLine()) != null) {  
-            	String string[] = line.toLowerCase().split("([,.\\s]+)");  
-			System.out.println("after reading");
-            	//Adding all words generated in previous step into words  
-            	for(String s : string){  
-   	 	if (articles.contains(s))
-   		 continue;
-   	 	else
-               		 words.add(s);  
-            	}  
-        	}
-         	 
-        	//Determine the most repeated word in a file  
-        	for(int i = 0; i < words.size(); i++){  
-            	count = 1;  
-            	//Count each word in the file and store it in variable count  
-            	for(int j = i+1; j < words.size(); j++){  
-                	if(words.get(i).equals(words.get(j))){  
-                    	count++;  
-                	}   
-            	}  
-            	//If maxCount is less than count then store value of count in maxCount   
-            	//and corresponding word to variable word  
-            	if(count > maxCount){  
-                	maxCount = count;  
-                	word = words.get(i);  
-            	}  
-        	}  
-         	 
-        	System.out.println("Most repeated word: " + word);  
-        	br.close();  
-    	}  
-	}
+        	
+		//Opens file in read mode  
+		import java.io.FileReader;  
+    public static void main(String args[])throws Exception{    
+          FileReader fr=new FileReader("/home/ubuntu/recipe.txt");    
+          int i;    
+          while((i=fr.read())!=-1)    
+          System.out.print((char)i);    
+          fr.close();    
+}    
+	
